75b0804305562afa42cc154b448c705f4bceb1fd
dhosptn <ahmadridhoseptian388@gmail.com>
2025-04-27 01:21:59 +0700

    first commit

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
 });
